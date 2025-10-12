aa243b4b735d0ec54b8491097abe7153b5cd732d
evilroky <vitaldj228@gmail.com>
2025-10-12 13:01:43 +0300

    Initial commit

---
+++ src/test/java/ru/egor/testingapp/TestingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egor.testingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
